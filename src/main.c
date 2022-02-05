1b9944d903238bcb682c07a8f7b44e5e44a19744
matsu <pineshrine@gmail.com>
2022-02-06 03:06:14 +0900

    add config hhmmss via cli, timer is to be coded

--- src/main.c
+++ src/main.c
@@ -1,12 +1,15 @@
 /**
- * Copyright (C) PlatformIO <contact@platformio.org>
- * See LICENSE for details.
+ * https://github.com/pineshrine/uno-7s595-1/
+ * author : Matsu @pineshrine
+ * just as hobby work at midnight
  */
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 const int map7seg[11][8] =
 {
@@ -23,6 +26,27 @@ const int map7seg[11][8] =
     {0,0,0,0,0,0,1,1}
 };
 
+struct time_time
+{
+    int hour;
+    int minute;
+    int second;
+};
+
+struct time_time time_timer;
+
+
+volatile unsigned char input[8];
+volatile unsigned char input_length;
+
+void dr_595(unsigned int d1_value, unsigned int d2_value, unsigned int d3_value, unsigned int d4_value, unsigned int colon);
+void display_quad7seg(unsigned int dis_value);
+void send_char(unsigned char data);
+void send_msg(unsigned char *str);
+void send_msg_r(unsigned char *str);
+void send_msg_n(unsigned char *str);
+
+//intended for drive 74HC595
 void dr_595(unsigned int d1_value, unsigned int d2_value, unsigned int d3_value, unsigned int d4_value, unsigned int colon)
 {
     cli();
@@ -151,6 +175,7 @@ void dr_595(unsigned int d1_value, unsigned int d2_value, unsigned int d3_value,
 
 }
 
+//display 4 digits value on OSL40391-IG quad7seg-led array via dr_595 func
 void display_quad7seg(unsigned int dis_value)
 {
     int d1 = dis_value / 1000;
@@ -171,6 +196,7 @@ void display_quad7seg(unsigned int dis_value)
     }
 }
 
+//send atomic char to usart(arduino uno way)
 void send_char(unsigned char data)
 {
 	while(!(UCSR0A & (1<<UDRE0)));
@@ -207,6 +233,41 @@ void send_msg_n(unsigned char *str)
     send_char(13);
 }
 
+ISR(USART_RX_vect)
+{
+    unsigned char buf = UDR0;
+    if (buf != '\n')
+    {
+        input[input_length++] = buf;
+    }
+    if (input_length >= 7 || buf == '\n')
+    {
+        //a little bit tricky cuz I'm not good to code ^^;
+        //there is a more better way
+        unsigned long hhmmss;
+        char *e;
+        send_msg_n(input);
+        char *ret[32];
+
+        const char hoge1[] = "debug:";send_msg(hoge1);
+        hhmmss = strtoul(input,&e,10);
+        int hh = hhmmss / 10000;
+        int mm = (hhmmss / 100) % 100;
+        int ss = hhmmss % 100;
+        time_timer.hour = hh;
+        time_timer.minute = mm;
+        time_timer.second = ss;
+        sprintf(ret,"hr:%d",time_timer.hour);
+        send_msg_n(ret);
+        sprintf(ret,"mi:%d",time_timer.minute);
+        send_msg_n(ret);
+        sprintf(ret,"se:%d",time_timer.second);
+        send_msg_n(ret);
+
+        input_length = 0;
+    }
+}
+
 int main(void)
 {
     // HC595 drive
@@ -217,12 +278,17 @@ int main(void)
     //usart configuration for terminal monitor
     UBRR0 = ((F_CPU/(16UL*9600UL))-1); //9600 is BAUD, magic word
     UCSR0A = UCSR0B = UCSR0C = 0; // clear all registers
-    UCSR0B |= ((1<<TXEN0)|(1<<RXEN0));
+    UCSR0B |= ((1<<TXEN0)|(1<<RXEN0)|(1<<RXCIE0)); // enable tx,rx,rx interrupt
     UCSR0C |= ((1<<UCSZ01)|(1<<UCSZ00)); 
 
+    //timer configuration for clock intr
+    //WIP
+
     char *buf[128];
-    sprintf(buf,"%s","started.");
+
+    sprintf(buf,"%s","started. input HHMMSS:");
     send_msg_n(buf);
+
 
     while (1)
     {
@@ -230,8 +296,8 @@ int main(void)
         {
             display_quad7seg(i);
             _delay_us(10);
-            sprintf(buf,"%d ",i);
-            send_msg_r(buf);
+            //sprintf(buf,"%d ",i);
+            //send_msg_r(buf);
         }
     }
 
